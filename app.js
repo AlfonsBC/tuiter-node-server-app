fd5390b7d4fd6b9ca935bbeb3f2f2e01179a7dde
AlfonsBC <albace2000@hotmail.com>
2023-04-09 16:09:28 -0400

    a9 2023

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ import tuitsController from './controllers/tuits/tuits-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import mongoose from 'mongoose';
 const app = express();
-
-
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
